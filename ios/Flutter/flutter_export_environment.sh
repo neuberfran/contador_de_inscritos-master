4fdc01344e8d719a2904a3752ab336a626d9b958
neuberfran <neuberfran@gmail.com>
2020-02-10 14:55:06 -0300

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\neuber\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\neuber\FlutterProject\contador_de_inscritos-master"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\neuber\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
---
+++ lib/models/video_model.dart
@@ -0,0 +1,28 @@
+import 'package:equatable/equatable.dart';
+
+class VideoModel  extends Equatable {
+
+    String id;
+    String title;
+    String image;
+    int viewCount;
+    int likeCount;
+    int dislikeCount;
+
+    VideoModel({
+        this.id, this.viewCount, this.title,
+        this.likeCount, this.image, this.dislikeCount
+    }) : super([ id, viewCount, title,
+        likeCount, image, dislikeCount]);
+
+    VideoModel.fromJson(Map<String, dynamic> data)
+        : this.id = data['videoId'],
+            this.viewCount = int.parse(data['viewCount']),
+            this.title = data['title'],
+            this.image = data['url'],
+            this.likeCount = int.parse(data['likeCount']),
+            this.dislikeCount = int.parse(data['dislikeCount']);
+
+
+
+}
